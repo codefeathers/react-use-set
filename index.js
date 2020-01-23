7c985dc37b989aae74b4d62632a29b43e855161d
Muthu Kumar <muthukumar@thefeathers.in>
2020-01-23 17:47:20 +0530

    (fix) set not updated when React rerenders
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ import { useState } from "react";
 
 export const useSet = set => {
 
-	const [, setWrap] = useState([ set ]);
+	const [ [ theSet ], setWrap] = useState([ set ]);
 
 	const modifiers = new Set([ "add", "delete", "clear" ]);
 
-	const setProxy = new Proxy(set, {
+	const setProxy = new Proxy(theSet, {
 		get: (_, key) => {
 			if (key === "toggle") {
 				return value =>
-					set.has(value)
+					theSet.has(value)
 						? setProxy.delete(value)
 						: setProxy.add(value);
 			} else if (modifiers.has(key)) {
 				return value => {
-					set[key](value);
-					setWrap([ set ]);
+					theSet[key](value);
+					setWrap([ theSet ]);
 					return setProxy;
 				}
 			} else {
-				return set[key].bind(set);
+				return theSet[key].bind(theSet);
 			}
 
 		},
